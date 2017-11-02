d84417768f9b1b1bee160f286aef989e7fc3a470
peter.geertsema <iwannapjg@gmail.com>
2017-11-02 16:09:10 -0400

    Prepared plugin for deployment

--- src/com/inflectra/idea/ui/SpiraTeamLogin.java
+++ src/com/inflectra/idea/ui/SpiraTeamLogin.java
@@ -25,6 +25,7 @@ import com.intellij.openapi.project.Project;
 
 /**
  * Action which allows the user to log into SpiraTeam
+ * @author Peter Geertsema
  */
 public class SpiraTeamLogin extends AnAction {
 
